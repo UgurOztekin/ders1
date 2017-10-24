34f421b35604432c7ef19ea1bceba93a98cabdd1
UgurOztekin <grafabrix@gmail.com>
2017-10-24 21:24:33 +0300

    ilk yükleme
    asdasdasd

---
+++ emirdersler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emirdersler
+{
+    class Program
+    {
+
+        public class ogrenci
+        {
+            public string adi { get; set; }
+            public string soyadi { get; set; }
+        }
+
+       public static void Main(string[] args)
+        {
+            List<ogrenci> o = new List<ogrenci>() { new ogrenci
+            {
+                adi="Ali", soyadi="yılmaz"
+            }};
+
+            for (int i = 0; i < o.Count; i++)
+            {
+                Console.WriteLine(o[i].adi);
+                Console.ReadKey();
+            }
+            foreach (var item in o)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
